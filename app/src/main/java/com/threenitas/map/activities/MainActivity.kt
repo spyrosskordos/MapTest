07e2948f9538bb7a43673d1da0bb4104f2e8c101
spyrosskordos <1q2w3e4r5t>
2019-07-03 17:44:33 +0300

    sec

--- app/src/main/java/com/threenitas/map/activities/MainActivity.kt
+++ app/src/main/java/com/threenitas/map/activities/MainActivity.kt
@@ -7,9 +7,15 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.threenitas.map.fragments.HistoryFragment
 import com.threenitas.map.R
 import com.threenitas.map.fragments.MapFragment
@@ -21,6 +27,8 @@ class MainActivity : AppCompatActivity() {
     private val mapFragment: MapFragment
     private val historyFragment: HistoryFragment
 
+    lateinit var mapFrg : SupportMapFragment
+    lateinit var googleMap: GoogleMap
     init {
         mapFragment =MapFragment()
         historyFragment= HistoryFragment()
@@ -30,8 +38,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Configure action bar
+//    mapFrg=supportFragmentManager.findFragmentById(R.id.frg1) as SupportMapFragment
+//        mapFrg.getMapAsync(OnMapReadyCallback {
+//            googleMap = it
+//            Log.d("GoogleMap", "before isMyLocationEnabled")
+////            googleMap.isMyLocationEnabled = true
+//            val location1 = LatLng(13.0356745,77.5881522)
+//            googleMap.addMarker(MarkerOptions().position(location1).title("My Location"))
+//            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location1,5f))
+//
+//            Log.d("GoogleMap", "before location2")
+//            val location2 = LatLng(9.89,78.11)
+//            googleMap.addMarker(MarkerOptions().position(location2).title("Madurai"))
+//
+//            Log.d("GoogleMap", "before location3")
+//
+//            val location3 = LatLng(13.029727,77.5933021)
+//            googleMap.addMarker(MarkerOptions().position(location3).title("Bangalore"))
+//
+//
+//
+//
+//        })
+         //Configure action bar
         setSupportActionBar(toolbar)
         val actionBar = supportActionBar
 
--- app/src/main/java/com/threenitas/map/fragments/MapFragment.kt
+++ app/src/main/java/com/threenitas/map/fragments/MapFragment.kt
@@ -1,20 +1,18 @@
 package com.threenitas.map.fragments
 
 
+import android.app.Activity
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
+import android.location.LocationManager
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
@@ -28,13 +26,16 @@ import com.threenitas.map.models.Predictions
 import kotlinx.android.synthetic.main.fragment_map.*
 import android.widget.Toast
 import android.util.AttributeSet
+import android.util.Log
+import com.google.android.gms.maps.*
 import com.threenitas.map.adapters.PlacesAutoCompleteAdapter
+import kotlinx.android.synthetic.*
 
-class MapFragment : Fragment(),OnMapReadyCallback {
-  private var googleMap: GoogleMap?=null
-
+class MapFragment : Fragment(){
+    lateinit var googleMap: GoogleMap
+    lateinit var mapView: MapView
     lateinit var autoCompleteTextViewPlace: AutoCompleteTextView
-
+    private var locationManager : LocationManager? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,14 +52,30 @@ class MapFragment : Fragment(),OnMapReadyCallback {
 
         return rootView
     }
-    override fun onMapReady(googleMap: GoogleMap?) {
-        this.googleMap = googleMap!!
 
-    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         autoCompleteTextViewPlace = view!!.findViewById(R.id.autoCompleteTextViewPlace) as AutoCompleteTextView
+
+        mapView=view.findViewById<MapView>(R.id.map_view) as MapView
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(OnMapReadyCallback {
+        googleMap=it
+
+            Log.d("GoogleMap", "before isMyLocationEnabled")
+//
+            mapView.onResume()
+            val location1 = LatLng(13.0356745,77.5881522)
+            googleMap.addMarker(MarkerOptions().position(location1).title("My Location"))
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location1,5f))
+
+
+
+
+        })
         loadData()
     }
+
+
 
     private fun loadData() {
         val placesAutoCompleteAdapter: PlacesAutoCompleteAdapter
@@ -67,5 +84,6 @@ class MapFragment : Fragment(),OnMapReadyCallback {
 
         autoCompleteTextViewPlace.threshold = 1
         autoCompleteTextViewPlace.setAdapter(placesAutoCompleteAdapter)
+
     }
 }// Required empty public constructor
